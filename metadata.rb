d3339a49a6682be52011fe1ccf613bae5c327b14
William Mantly <wmantly@gmail.com>
2019-06-30 17:28:00 -0400

    added MySQL

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,12 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures t42-common'
 long_description 'Installs/Configures t42-common'
-version '0.1.4'
+version '0.1.5'
 chef_version '>= 13.0'
 
 depends 'nodejs'
 depends 'postgresql'
+depends 'mysql'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
