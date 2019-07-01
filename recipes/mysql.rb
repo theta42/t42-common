873a1de47e703b1c50842cb738cca3db87783061
William Mantly <wmantly@gmail.com>
2019-07-01 00:22:07 -0400

    MySQL syntax error

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -1,7 +1,7 @@
 mysql_service node['app']['name'] do
   # version '5.7'
   bind_address node['db']['bind_address']
-  port ['db']['port']
+  port node['db']['port']
   # data_dir '/data'
   initial_root_password node['db']['root_password']
 
@@ -11,9 +11,9 @@ end
 
 bash 'Make mysql Database and User' do
 	code <<~EOH
-		mysql -h 127.0.0.1 -uroot -p"#{node['db']['root_password']}" -e "CREATE DATABASE #{node['db']['name']} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-	    mysql -h 127.0.0.1 -uroot -p"#{node['db']['root_password']}" -e "CREATE USER #{node['db']['user']}@'%' IDENTIFIED BY '#{node['db']['password']}';"
-	    mysql -h 127.0.0.1 -uroot -p"#{node['db']['root_password']}" -e "GRANT ALL PRIVILEGES ON #{node['db']['name']}.* TO '#{node['db']['user']}'@'localhost';"
+		mysql -h 127.0.0.1 -uroot -p"#{node['db']['root_password']}" -e "CREATE DATABASE '#{node['db']['name']}' /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+	    mysql -h 127.0.0.1 -uroot -p"#{node['db']['root_password']}" -e "CREATE USER '#{node['db']['user']}'@'%' IDENTIFIED BY '#{node['db']['password']}';"
+	    mysql -h 127.0.0.1 -uroot -p"#{node['db']['root_password']}" -e "GRANT ALL PRIVILEGES ON '#{node['db']['name']}'.* TO '#{node['db']['user']}'@'localhost';"
 	    mysql -h 127.0.0.1 -uroot -p"#{node['db']['root_password']}" -e "FLUSH PRIVILEGES;"
 
 	EOH
